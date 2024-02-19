81d1463a0e41ffa939fc69cbd37caeed0e77b3c7
zihad868 <zihadh868@gmail.com>
2024-02-19 13:13:14 +0600

    Implements JavaScript Functionality

--- js/srcipt.js
+++ js/srcipt.js
@@ -1,1 +1,23 @@
-console.log("Hello JavaScript")
+const seatBtns = document.getElementsByClassName('seat-btn')
+
+for(let seatBtn of seatBtns){
+    seatCount = 8;
+    count = 0;
+    seatBtn.addEventListener('click', function(){
+        const seatNumber = document.getElementById('seatNumber');
+        let seatNumberStr = seatNumber.innerText;
+        let seatNumberInteger = parseInt(seatNumberStr);
+
+        // Seat Count Functionality 
+        seatCount = seatCount - 1
+        console.log(seatCount)
+        setOutPut('seatNumber', seatCount)
+        
+    })
+}
+
+
+function setOutPut(elementId, value){
+   let element = document.getElementById(elementId).innerText = value;
+   console.log(value);
+}
